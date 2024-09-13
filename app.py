35fb5c13903b22d21922c837b5bde3c751329559
swagatobhaskar <bhaskarswagato@gmail.com>
2024-09-13 17:01:46 +0530

    added created_at to template, added flask-migrate

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy.sql import func
+from flask_migrate import Migrate
 from os import path, environ, getenv
 from dotenv import load_dotenv
 
-# basedir = path.abspath(path.dirname(__file__))
-# load_dotenv(path.join(basedir, '.env'))
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
 
 app = Flask(__name__)
 
@@ -22,19 +23,16 @@ else:
 app.config.from_object(config)
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 """
 Without using flask-migrate, create the db with:
->>> flask shell
->>> from app import db, Todo
->>> db.create_all()
->>> Todo.query.all()
-
-With flask-migrate, run the commands:
->>> flask shell
->>> flask db init
->>> flask db migrate -m "..."
->>> flask db upgrade
+    flask shell
+    from app import db, Todo
+    db.create_all()
+    Todo.query.all()
+    exit()
+Flask-Migrate can be also added after this point.
 """
 # ---------------------------------------------------------------------
 class Todo(db.Model):
@@ -42,9 +40,14 @@ class Todo(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     item = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now(), nullable=True)
+    
+    def verbose_time(self):
+        # %d- Numeric day of the month, %B Month full name
+        return self.created_at.strftime("%d %B, %Y at %H:%M %p")
 
     def __repr__(self):
-        return f'<Todo item: {str(self.id)}>'
+        return f'<Todo ID: {str(self.id)} | {self.item[:10]}>'
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -68,7 +71,6 @@ def remove_item(id):
     return jsonify({'message': 'success'}), 200
 
 # ----------------------------------------------------------------------
-
 
 if __name__=='__main__':
     # removed DEBUG=True because it will be modified through config file
